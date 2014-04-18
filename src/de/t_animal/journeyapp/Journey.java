75349af24f660d2bc5975cd2c764ad89ee32e325
t.animal <ar79yxiw@cip.cs.fau.de>
2014-04-18 22:39:18 +0200

    Add raw android files

---
+++ src/de/t_animal/journeyapp/Journey.java
@@ -0,0 +1,22 @@
+package de.t_animal.journeyapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Journey extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_journey);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.journey, menu);
+		return true;
+	}
+
+}
