d1f6c8cdaac28c4f79ec51f6d8754430b4261336
t.animal <ar79yxiw@cip.cs.fau.de>
2014-04-22 15:58:38 +0200

    Parse data in server application

--- journeyServer.py
+++ journeyServer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python2
 
 import socket
+import datetime
+import struct
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -10,5 +12,9 @@ print "set up"
 
 while True:
 		data, addr = sock.recvfrom(1024)
-		print "connection handled"
-		print "Data: {} ".format(data)
+		userIdLen = ord(data[0])
+		userId = data[1:userIdLen+1]
+		lat, lon, acc = struct.unpack(">ddf", data[userIdLen+1:])
+		
+		print "[{}, {: <26s}] ".format(str(datetime.datetime.now()), str(addr)),
+		print "User: {} Lat: {:0<3.7f} Lon: {:0<3.7f} Acc:{} ".format(userId, lat, lon, acc)
