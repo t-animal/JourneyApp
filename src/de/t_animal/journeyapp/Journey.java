af0a7dcd222002ce54a8a187ea1934bb597751b6
t.animal <ar79yxiw@cip.cs.fau.de>
2014-04-22 23:22:26 +0200

    Add tabs to main view (screw feature branches!)

--- src/de/t_animal/journeyapp/Journey.java
+++ src/de/t_animal/journeyapp/Journey.java
@@ -3,16 +3,33 @@ package de.t_animal.journeyapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar.Tab;
+import android.support.v7.app.ActionBar.TabListener;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ToggleButton;
 
-public class Journey extends Activity {
+public class Journey extends ActionBarActivity implements TabListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_journey);
+
+		ActionBar actionBar = getSupportActionBar();
+
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		actionBar.addTab(actionBar.newTab()
+				.setText(R.string.tab_map_name)
+				.setTabListener(this));
+		actionBar.addTab(actionBar.newTab()
+				.setText(R.string.tab_info_name)
+				.setTabListener(this));
+
 	}
 
 	@Override
@@ -21,13 +38,27 @@ public class Journey extends Activity {
 		getMenuInflater().inflate(R.menu.journey, menu);
 		return true;
 	}
-	
-	public void onToggleLocationService(View view){
-		if(((ToggleButton) view).isChecked()){
+
+	public void onToggleLocationService(View view) {
+		if (((ToggleButton) view).isChecked()) {
 			startService(new Intent(this, LocationService.class));
-		}else{
+		} else {
 			stopService(new Intent(this, LocationService.class));
 		}
+	}
+
+	@Override
+	public void onTabSelected(Tab arg0, FragmentTransaction arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onTabReselected(Tab arg0, FragmentTransaction arg1) {
+	}
+
+	@Override
+	public void onTabUnselected(Tab arg0, FragmentTransaction arg1) {
 	}
 
 }
