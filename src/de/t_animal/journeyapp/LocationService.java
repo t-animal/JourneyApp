abd84362b8da1839d745a6bb1219439f60d90cda
t.animal <ar79yxiw@cip.cs.fau.de>
2014-04-22 22:29:56 +0200

    Add a simple notification that keeps the service in foreground

--- src/de/t_animal/journeyapp/LocationService.java
+++ src/de/t_animal/journeyapp/LocationService.java
@@ -1,7 +1,9 @@
 package de.t_animal.journeyapp;
 
 import android.app.IntentService;
+import android.app.Notification;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 public class LocationService extends IntentService {
@@ -16,6 +18,13 @@ public class LocationService extends IntentService {
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
+
+		Notification foregroundNotification = new NotificationCompat.Builder(this)
+				.setSmallIcon(R.drawable.ic_launcher)
+				.setContentTitle(getResources().getString(R.string.notificationTitle))
+				.setContentText(getResources().getString(R.string.notificationMessage)).build();
+
+		startForeground(1, foregroundNotification);
 
 		return super.onStartCommand(intent, flags, startId);
 	}
@@ -33,15 +42,14 @@ public class LocationService extends IntentService {
 		System.out.println("Intent received");
 
 		while (!isStopped) {
-				
-				try {
-					Thread.sleep(5000);
-				} catch (InterruptedException e) {
-					//do nothing, just execute as usual
-				}
 
-				System.out.println("Service still running");
-				
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				// do nothing, just execute as usual
+			}
+
+			System.out.println("Service still running");
 
 		}
 	}
