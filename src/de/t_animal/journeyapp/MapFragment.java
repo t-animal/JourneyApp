8c63c860ea1dd224289404870bf48bedca80b4fd
t.animal <ar79yxiw@cip.cs.fau.de>
2014-04-25 17:29:30 +0200

    Add communication object to map

--- src/de/t_animal/journeyapp/MapFragment.java
+++ src/de/t_animal/journeyapp/MapFragment.java
@@ -5,9 +5,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 
 public class MapFragment extends Fragment {
+
+	WebView map;
+
+	/**
+	 * A class to be inserted into the JS and act as a communication bridge through it's getters and setters
+	 */
+	private class CommunicationObject {
+
+		String theme;
+
+		public void setTheme(String theme) {
+			this.theme = theme;
+		}
+
+		@JavascriptInterface
+		public String getTheme() {
+			return this.theme;
+		}
+	}
+
+	CommunicationObject co = new CommunicationObject();
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -15,9 +37,11 @@ public class MapFragment extends Fragment {
 		// inflat and return the layout
 		View fragmentRootView = inflater.inflate(R.layout.fragment_map, container, false);
 
-		WebView map = (WebView) fragmentRootView.findViewById(R.id.map_view);
+		map = (WebView) fragmentRootView.findViewById(R.id.map_view);
 
 		map.getSettings().setJavaScriptEnabled(true);
+		map.addJavascriptInterface(co, "comm");
+
 		map.loadUrl("file:///android_asset/map.html");
 
 		return fragmentRootView;
