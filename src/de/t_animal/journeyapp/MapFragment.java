c0c3d6d180710445299cbdbba4916dd4dba2f7df
t.animal <ar79yxiw@cip.cs.fau.de>
2014-04-27 21:29:01 +0200

    Draw map overlays from kml file

--- src/de/t_animal/journeyapp/MapFragment.java
+++ src/de/t_animal/journeyapp/MapFragment.java
@@ -5,37 +5,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 
 public class MapFragment extends Fragment {
 
 	WebView map;
 
-	/**
-	 * A class to be inserted into the JS and act as a communication bridge through it's getters and setters
-	 */
-	private class CommunicationObject {
-
-		String theme;
-
-		public void setTheme(String theme) {
-			this.theme = theme;
-		}
-
-		@JavascriptInterface
-		public String getTheme() {
-			return this.theme;
-		}
-	}
-
-	CommunicationObject co = new CommunicationObject();
+	JSCommunicationObject co;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// inflat and return the layout
 		View fragmentRootView = inflater.inflate(R.layout.fragment_map, container, false);
+
+		co = new JSCommunicationObject(this.getActivity());
 
 		map = (WebView) fragmentRootView.findViewById(R.id.map_view);
 
